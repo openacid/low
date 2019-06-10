f17dba317473b480ffa7d13b8afd3fc641f3f17e
drdr xp <drdr.xp@gmail.com>
2019-06-10 21:23:05 +0800

    refactor: bitword: complete test coverage of FirstDiff

--- bitword/bitword_test.go
+++ bitword/bitword_test.go
@@ -231,6 +231,7 @@ func TestFirstDiff(t *testing.T) {
 
 		{"aac", "aa", 4, 0, 4, 4},
 		{"aac", "ab", 4, 0, 4, 3},
+		{"aac", "ab", 4, 0, 100, 3},
 
 		// bug in 0.1.2
 		{"aaa", "aaa", 4, 0, -1, 6},
