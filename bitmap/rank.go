08a451956b851d753dc01271545d0b4fd12a65e9
drdr xp <drdr.xp@gmail.com>
2019-06-21 10:17:47 +0800

    refactor: bitmap: move out rank to separate files

---
+++ bitmap/rank.go
@@ -0,0 +1,59 @@
+package bitmap
+
+import (
+	"math/bits"
+)
+
+// IndexRank64 creates a rank index for a bitmap.
+// rank(i) is defined as number of "1" upto position i, excluding i.
+//
+// It returns an index of []int32.
+// Every element in it is rank(i*64)
+//
+// Since 0.1.8
+func IndexRank64(words []uint64) []int32 {
+
+	idx := make([]int32, len(words))
+	n := int32(0)
+	for i := 0; i < len(words); i++ {
+		idx[i] = n
+		n += int32(bits.OnesCount64(words[i]))
+	}
+
+	return idx
+}
+
+// IndexRank128 creates a rank index for a bitmap.
+// rank(i) is defined as number of "1" upto position i, excluding i.
+//
+// It returns an index of []int32.
+// Every element in it is rank(i*128).
+//
+// It also adds a last index item if len(words) % 2 == 0, in order to make the
+// distance from any bit to the closest index be less than 64.
+//
+//
+// Since 0.1.8
+func IndexRank128(words []uint64) []int32 {
+
+	idx := make([]int32, 0)
+	n := int32(0)
+	for i := 0; i < len(words); i += 2 {
+		idx = append(idx, n)
+		n += int32(bits.OnesCount64(words[i]))
+		if i < len(words)-1 {
+			n += int32(bits.OnesCount64(words[i+1]))
+		}
+	}
+
+	// Need a last index to let distance from every bit to its closest index
+	// <=64
+	if len(words)&1 == 0 {
+		idx = append(idx, n)
+	}
+
+	// clone to reduce cap to len
+	idx = append(idx[:0:0], idx...)
+
+	return idx
+}
---
+++ bitmap/rank_test.go
@@ -0,0 +1,68 @@
+package bitmap
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestIndexRank(t *testing.T) {
+
+	ta := require.New(t)
+
+	cases := []struct {
+		bm      []uint64
+		want64  []int32
+		want128 []int32
+	}{
+		{
+			[]uint64{},
+			[]int32{},
+			[]int32{0},
+		},
+		{
+			[]uint64{0},
+			[]int32{0},
+			[]int32{0},
+		},
+		{
+			[]uint64{1},
+			[]int32{0},
+			[]int32{0},
+		},
+		{
+			[]uint64{0xffffffffffffffff},
+			[]int32{0},
+			[]int32{0},
+		},
+		{
+			[]uint64{0xffffffffffffffff, 1},
+			[]int32{0, 64},
+			[]int32{0, 65},
+		},
+		{
+			[]uint64{0xffffffffffffffff, 1, 1},
+			[]int32{0, 64, 65},
+			[]int32{0, 65},
+		},
+		{
+			[]uint64{0xffffffffffffffff, 1, 1, 3},
+			[]int32{0, 64, 65, 66},
+			[]int32{0, 65, 68},
+		},
+		{
+			[]uint64{0xffffffffffffffff, 1, 1, 3, 4},
+			[]int32{0, 64, 65, 66, 68},
+			[]int32{0, 65, 68},
+		},
+	}
+
+	for i, c := range cases {
+
+		idx64 := IndexRank64(c.bm)
+		ta.Equal(c.want64, idx64, "%d-th: case: %+v", i+1, c)
+
+		idx128 := IndexRank128(c.bm)
+		ta.Equal(c.want128, idx128, "%d-th: case: %+v", i+1, c)
+	}
+}
